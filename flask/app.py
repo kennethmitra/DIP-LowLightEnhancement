44535f9c704a9ef8ca36547d87077f9e3aea8fc9
Jesse Zhang <43320266+sophoros@users.noreply.github.com>
2020-11-21 15:42:33 -0600

    Basic uploading capability

--- flask/app.py
+++ flask/app.py
@@ -1,6 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_uploads import UploadSet, configure_uploads, IMAGES
 
 app = Flask(__name__)
+
+
+images = UploadSet('images', IMAGES)
+
+app.config['UPLOADED_IMAGES_DEST'] = '/uploads'
+configure_uploads(app, (images,))
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST' and 'image' in request.files:
+        filename = images.save(request.files['image'])
+        return filename
+    return render_template('index.html')
 
 
 @app.route('/')
