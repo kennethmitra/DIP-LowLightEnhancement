f62ef88a5d34978713af9292837652a770951739
Jesse Zhang <43320266+sophoros@users.noreply.github.com>
2020-11-22 03:39:08 -0600

    +++

--- flask/app.py
+++ flask/app.py
@@ -1,20 +1,39 @@
-from flask import Flask, render_template, request
-from flask_uploads import UploadSet, configure_uploads, IMAGES
+from flask import Flask, render_template, request, redirect
+from werkzeug.utils import secure_filename
+import os
+import sys
 
 app = Flask(__name__)
 
+app.config['IMAGE_UPLOADS'] = './static/uploads'
+app.config['ALLOWED_IMAGE_EXTENSIONS'] = ['JPEG', 'JPG', 'PNG', 'GIF']
 
-images = UploadSet('images', IMAGES)
 
-app.config['UPLOADED_IMAGES_DEST'] = '/uploads'
-configure_uploads(app, (images,))
+def allowed_image(filename):
+    if not "." in filename:
+        return False
+    ext = filename.rsplit(".", 1)[1]
+    if ext.upper() in app.config["ALLOWED_IMAGE_EXTENSIONS"]:
+        return True
+    else:
+        return False
 
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
-    if request.method == 'POST' and 'image' in request.files:
-        filename = images.save(request.files['image'])
-        return filename
+    if request.method == 'POST':
+        if request.files:
+            image = request.files['image']
+            if image.filename == "":
+                print("No filename")
+                return redirect(request.url)
+            if allowed_image(image.filename):
+                filename = secure_filename(image.filename)
+                image.save(os.path.join(app.config['IMAGE_UPLOADS'], filename))
+                return render_template('index.html', image_org=filename)
+            else:
+                print("That file extension is not allowed")
+                return redirect(request.url)
     return render_template('index.html')
 
 
